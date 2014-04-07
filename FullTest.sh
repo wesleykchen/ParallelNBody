ae87ad13bff4eea77596d4f8cb2de8ae0357cd3a
Wesley Chen <wesleychen@college.harvard.edu>
2014-04-07 13:33:53 -0400

    revised script to test

--- FullTest.sh
+++ FullTest.sh
@@ -4,15 +4,11 @@ mpirun -n 16 ./symmetric -c 4 data/oneD_source_1000 data/oneD_charge_1000
 
 mpirun -n 16 ./teamscatter -c 4 data/oneD_source_1000 data/oneD_charge_1000
 
-mpirun -n 16 ./broadcast data/oneD_source_1000 data/oneD_charge_1000
-
 ./serial data/oneD_source_1000 data/oneD_charge_1000
 
 mpirun -n 16 ./symmetric -c 4 data/oneD_source_10000 data/oneD_charge_10000
 
 mpirun -n 16 ./teamscatter -c 4 data/oneD_source_10000 data/oneD_charge_10000
-
-mpirun -n 16 ./broadcast data/oneD_source_10000 data/oneD_charge_10000
 
 ./serial data/oneD_source_10000 data/oneD_charge_10000
 
@@ -20,7 +16,4 @@ mpirun -n 16 ./symmetric -c 4 data/oneD_source_100000 data/oneD_charge_100000
 
 mpirun -n 16 ./teamscatter -c 4 data/oneD_source_100000 data/oneD_charge_100000
 
-mpirun -n 16 ./broadcast data/oneD_source_100000 data/oneD_charge_100000
-
 ./serial data/oneD_source_100000 data/oneD_charge_100000
-
