fd5bc3e49789473da6ec26c706db2d27dd654b05
Wesley Chen <wesleychen@college.harvard.edu>
2014-04-07 13:30:15 -0400

    new scrip for full testing

--- FullTest.sh
+++ FullTest.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-mpirun -n 16 ./symmetric -c 2 data/oneD_source_10000 data/oneD_charge_10000
+mpirun -n 16 ./symmetric -c 4 data/oneD_source_1000 data/oneD_charge_1000
 
-mpirun -n 16 ./symmetric -c 2 data/oneD_source_100000 data/oneD_charge_100000
+mpirun -n 16 ./teamscatter -c 4 data/oneD_source_1000 data/oneD_charge_1000
 
-mpirun -n 8 ./symmetric -c 2 data/oneD_source_100000 data/oneD_charge_10000
+mpirun -n 16 ./broadcast data/oneD_source_1000 data/oneD_charge_1000
 
-mpirun -n 8 ./symmetric -c 2 data/oneD_source_100000 data/oneD_charge_100000
+./serial data/oneD_source_1000 data/oneD_charge_1000
+
+mpirun -n 16 ./symmetric -c 4 data/oneD_source_10000 data/oneD_charge_10000
+
+mpirun -n 16 ./teamscatter -c 4 data/oneD_source_10000 data/oneD_charge_10000
+
+mpirun -n 16 ./broadcast data/oneD_source_10000 data/oneD_charge_10000
+
+./serial data/oneD_source_10000 data/oneD_charge_10000
+
+mpirun -n 16 ./symmetric -c 4 data/oneD_source_100000 data/oneD_charge_100000
+
+mpirun -n 16 ./teamscatter -c 4 data/oneD_source_100000 data/oneD_charge_100000
+
+mpirun -n 16 ./broadcast data/oneD_source_100000 data/oneD_charge_100000
+
+./serial data/oneD_source_100000 data/oneD_charge_100000
 
