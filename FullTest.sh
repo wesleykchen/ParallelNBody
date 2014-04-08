ab67065821af8b2f800ce8739af68f78c0aec58a
Ubuntu <wesley@wesley-VirtualBox.(none)>
2014-04-07 23:22:42 -0400

    added benchmark script, no exe

--- FullTest.sh
+++ FullTest.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
-mpirun -n 8 ./symmetric -c 2 data/oneD_source_1000 data/oneD_charge_1000
+mpirun -n 8 ./symmetric 1000 -c 2
 
-mpirun -n 8 ./teamscatter -c 2 data/oneD_source_1000 data/oneD_charge_1000
+mpirun -n 8 ./teamscatter 1000 -c 2
 
-./serial data/oneD_source_1000 data/oneD_charge_1000
+./serial 1000
 
-mpirun -n 16 ./symmetric -c 4 data/oneD_source_10000 data/oneD_charge_10000
+mpirun -n 8 ./symmetric 10000 -c 2
 
-mpirun -n 16 ./teamscatter -c 4 data/oneD_source_10000 data/oneD_charge_10000
+mpirun -n 8 ./teamscatter 10000 -c 2
 
-./serial data/oneD_source_10000 data/oneD_charge_10000
+mpirun -n 16 ./symmetric 10000 -c 2
 
-mpirun -n 16 ./symmetric -c 4 data/oneD_source_100000 data/oneD_charge_100000
+mpirun -n 16 ./teamscatter 10000 -c 2
 
-mpirun -n 16 ./teamscatter -c 4 data/oneD_source_100000 data/oneD_charge_100000
+mpirun -n 16 ./symmetric 10000 -c 4
 
-./serial data/oneD_source_100000 data/oneD_charge_100000
+mpirun -n 16 ./teamscatter 10000 -c 4
+
+./serial 10000 -nocheck
+
+mpirun -n 8 ./symmetric 100000 -c 2 -nocheck
+
+mpirun -n 8 ./teamscatter 100000 -c 2 -nocheck
+
+mpirun -n 16 ./symmetric 100000 -c 2 -nocheck
+
+mpirun -n 16 ./teamscatter 100000 -c 2 -nocheck
+
+mpirun -n 16 ./symmetric 100000 -c 4 -nocheck
+
+mpirun -n 16 ./teamscatter 100000 -c 4 -nocheck
+
+./serial 100000 -nocheck
+
+
