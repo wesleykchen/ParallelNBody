b1e473ffc92f3c292930b0bdde445a628d774d80
Wesley Chen <wesleychen@college.harvard.edu>
2014-04-28 17:44:17 -0400

    updated test script to include c=1

--- FullTest.sh
+++ FullTest.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+mpirun -n 8 ./symmetric 1000 -c 1
+
+mpirun -n 8 ./teamscatter 1000 -c 1
+
 mpirun -n 8 ./symmetric 1000 -c 2
 
 mpirun -n 8 ./teamscatter 1000 -c 2
 
 ./serial 1000
 
+mpirun -n 8 ./symmetric 10000 -c 1
+
+mpirun -n 8 ./teamscatter 10000 -c 1
+
 mpirun -n 8 ./symmetric 10000 -c 2
 
 mpirun -n 8 ./teamscatter 10000 -c 2
+
+mpirun -n 16 ./symmetric 10000 -c 1
+
+mpirun -n 16 ./teamscatter 10000 -c 1
 
 mpirun -n 16 ./symmetric 10000 -c 2
 
@@ -20,9 +32,17 @@ mpirun -n 16 ./teamscatter 10000 -c 4
 
 ./serial 10000 -nocheck
 
+mpirun -n 8 ./symmetric 100000 -c 1 -nocheck
+
+mpirun -n 8 ./teamscatter 100000 -c 1 -nocheck
+
 mpirun -n 8 ./symmetric 100000 -c 2 -nocheck
 
 mpirun -n 8 ./teamscatter 100000 -c 2 -nocheck
+
+mpirun -n 16 ./symmetric 100000 -c 1 -nocheck
+
+mpirun -n 16 ./teamscatter 100000 -c 1 -nocheck
 
 mpirun -n 16 ./symmetric 100000 -c 2 -nocheck
 
