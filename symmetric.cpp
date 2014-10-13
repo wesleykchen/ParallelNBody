926315b0e5e7f7efc8a3e42afb0dcef1c6f28a50
Wesley Chen <wesleychen@rclogin07.rc.fas.harvard.edu>
2014-10-13 13:46:04 -0400

    onClusterChanges

--- symmetric.cpp
+++ symmetric.cpp
@@ -5,7 +5,7 @@
 
 #include "Util.hpp"
 
-#include "kernel/InvSq.kern"
+#include "kernel/NonParaBayesian.kern"
 #include "meta/kernel_traits.hpp"
 #include "meta/random.hpp"
 
@@ -108,8 +108,8 @@ int main(int argc, char** argv)
   // Scratch status for MPI
   MPI_Status status;
 
-  typedef InvSq kernel_type;
-  kernel_type K;
+  typedef NonParaBayesian kernel_type;
+  kernel_type K(1,1);
 
   // Define source_type, target_type, charge_type, result_type
   typedef kernel_type::source_type source_type;
