d293d012ab5b121b96cd1cdd182bf8906c11e491
Wesley Chen <wesleychen@rclogin03.rc.fas.harvard.edu>
2014-10-14 02:15:10 -0400

    Argh Bug and Odyssey

--- oneRunScript.sh
+++ oneRunScript.sh
@@ -4,8 +4,8 @@
 #SBATCH -p general   	      #Partition to submit to
 
 #SBATCH --mem-per-cpu=2000     #Memory per cpu in MB (see also --mem)
-#SBATCH -o data.out     	      #File to which standard out will be written
-#SBATCH -e d.err      	      #File to which standard err will be written
+#SBATCH -o n25600p64c8.out     	      #File to which standard out will be written
+#SBATCH -e n25600p64c8.err      	      #File to which standard err will be written
  
 #
 # Use modules to setup the runtime environment
@@ -18,25 +18,25 @@ module load centos6/openmpi-1.6.5_gcc-4.8.0
 # Execute the run
 #
 
-mpirun -np 64  ./symmetric 25600  -c 1 #-nocheck
+#mpirun -np 64  ./symmetric 25600  -c 1 -nocheck
 
-mpirun -np 64  ./symmetric 25600  -c 2 #-nocheck
+#mpirun -np 64  ./symmetric 25600  -c 2 #-nocheck
 
-mpirun -np 64  ./symmetric 25600  -c 4 #-nocheck
+#mpirun -np 64  ./symmetric 25600  -c 4 #-nocheck
 
 mpirun -np 64  ./symmetric 25600  -c 8 #-nocheck
 
-mpirun -np 32  ./symmetric 25600  -c 1 #-nocheck
+#mpirun -np 32  ./symmetric 25600  -c 1 #-nocheck
 
-mpirun -np 32  ./symmetric 25600  -c 2 #-nocheck
+#mpirun -np 32  ./symmetric 25600  -c 2 #-nocheck
 
-mpirun -np 32  ./symmetric 25600  -c 4 #-nocheck
+#mpirun -np 32  ./symmetric 25600  -c 4 #-nocheck
 
-mpirun -np 16  ./symmetric 25600  -c 1 #-nocheck
+#mpirun -np 16  ./symmetric 25600  -c 1 #-nocheck
 
-mpirun -np 16  ./symmetric 25600  -c 2 #-nocheck
+#mpirun -np 16  ./symmetric 25600  -c 2 #-nocheck
 
-mpirun -np 16  ./symmetric 25600  -c 4 #-nocheck
+#mpirun -np 16  ./symmetric 25600  -c 4 #-nocheck
 
 #mpirun -np 32  ./teamscatter 25600  -c 4
 
