9014bd7cff38be1b292aa2b7d7294e6082147076
Wesley Chen <wesleychen@college.harvard.edu>
2014-10-18 12:35:46 -0400

    lets go

--- stampedeLargeScaleAll.sh
+++ stampedeLargeScaleAll.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --ntasks 4096         #Number of processes
+#SBATCH --ntasks 2048         #Number of processes
 #SBATCH -t 03:00:00                #Runtime in minutes
 #SBATCH -p normal   	      #Partition to submit to
-#SBATCH -o data/stampedefull512k.out     	      #File to which standard out will be written
-#SBATCH -e data/stampedefull512k.err      	      #File to which standard err will be written
+#SBATCH -o data/a_stampedefullTS256k.out     	      #File to which standard out will be written
+#SBATCH -e data/a_stampedefullTS256k.err      	      #File to which standard err will be written
 
 module swap intel gcc/4.7.1
 
@@ -14,100 +14,86 @@ make symmetric XFLAGS='-DP2P_NUM_THREADS=0 -DP2P_DECAY_ITERATOR=0'
 #
 # Execute the run
 #
-ibrun -n 1  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 1  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 2  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 2  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 4  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 4  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 4  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 4  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 8  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 8  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 8  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 8  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 16  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 16  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 16  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 16  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 16  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 16  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 32  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 32  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 32  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 32  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 32  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 32  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 64  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 64  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 64  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 64  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 64  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 64  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 64  -o 0 ./symmetric 512000  -c 8 #-nocheck
+ibrun -n 64  -o 0 ./symmetric 256000  -c 8 #-nocheck
 
-ibrun -n 128  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 128  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 128  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 128  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 128  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 128  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 128  -o 0 ./symmetric 512000  -c 8 #-nocheck
+ibrun -n 128  -o 0 ./symmetric 256000  -c 8 #-nocheck
 
-ibrun -n 256  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 256  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 256  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 256  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 256  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 256  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 256  -o 0 ./symmetric 512000  -c 8 #-nocheck
+ibrun -n 256  -o 0 ./symmetric 256000  -c 8 #-nocheck
 
-ibrun -n 256  -o 0 ./symmetric 512000  -c 16 #-nocheck
+ibrun -n 256  -o 0 ./symmetric 256000  -c 16 #-nocheck
 
-ibrun -n 512  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 512  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 512  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 512  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 512  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 512  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 512  -o 0 ./symmetric 512000  -c 8 #-nocheck
+ibrun -n 512  -o 0 ./symmetric 256000  -c 8 #-nocheck
 
-ibrun -n 512  -o 0 ./symmetric 512000  -c 16 #-nocheck
+ibrun -n 512  -o 0 ./symmetric 256000  -c 16 #-nocheck
 
-ibrun -n 1024  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 1024  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 1024  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 1024  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 1024  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 1024  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 1024  -o 0 ./symmetric 512000  -c 8 #-nocheck
+ibrun -n 1024  -o 0 ./symmetric 256000  -c 8 #-nocheck
 
-ibrun -n 1024  -o 0 ./symmetric 512000  -c 16 #-nocheck
+ibrun -n 1024  -o 0 ./symmetric 256000  -c 16 #-nocheck
 
-ibrun -n 1024  -o 0 ./symmetric 512000  -c 32 #-nocheck
+ibrun -n 1024  -o 0 ./symmetric 256000  -c 32 #-nocheck
 
-ibrun -n 2048  -o 0 ./symmetric 512000  -c 1 #-nocheck
+ibrun -n 2048  -o 0 ./symmetric 256000  -c 1 #-nocheck
 
-ibrun -n 2048  -o 0 ./symmetric 512000  -c 2 #-nocheck
+ibrun -n 2048  -o 0 ./symmetric 256000  -c 2 #-nocheck
 
-ibrun -n 2048  -o 0 ./symmetric 512000  -c 4 #-nocheck
+ibrun -n 2048  -o 0 ./symmetric 256000  -c 4 #-nocheck
 
-ibrun -n 2048  -o 0 ./symmetric 512000  -c 8 #-nocheck
+ibrun -n 2048  -o 0 ./symmetric 256000  -c 8 #-nocheck
 
-ibrun -n 2048  -o 0 ./symmetric 512000  -c 16 #-nocheck
+ibrun -n 2048  -o 0 ./symmetric 256000  -c 16 #-nocheck
 
-ibrun -n 2048  -o 0 ./symmetric 512000  -c 32 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 1 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 2 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 4 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 8 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 16 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 32 #-nocheck
-
-ibrun -n 4096  -o 0 ./symmetric 512000  -c 64 #-nocheck
+ibrun -n 2048  -o 0 ./symmetric 256000  -c 32 #-nocheck
--- stampedeLargeScaleTS.sh
+++ stampedeLargeScaleTS.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --ntasks 4096         #Number of processes
+#SBATCH --ntasks 2048         #Number of processes
 #SBATCH -t 01:00:00              #Runtime in minutes
 #SBATCH -p normal   	      #Partition to submit to
-#SBATCH -o data/stampedeTS256k.out     	      #File to which standard out will be written
-#SBATCH -e data/stampedeTS256k.err      	      #File to which standard err will be written
+#SBATCH -o data/a_stampedeTS5256k.out     	      #File to which standard out will be written
+#SBATCH -e data/a_stampedeTS5256k.err      	      #File to which standard err will be written
 
 module swap intel gcc/4.7.1
 	
@@ -15,16 +15,14 @@ make teamscatter XFLAGS='-DP2P_NUM_THREADS=0 -DP2P_DECAY_ITERATOR=0'
 # Execute the run
 #
 
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 1 #-nocheck
+ibrun -n 2048  -o 0 ./teamscatter 256000  -c 1 #-nocheck
 
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 2 #-nocheck
+ibrun -n 2048  -o 0 ./teamscatter 256000  -c 2 #-nocheck
 
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 4 #-nocheck
+ibrun -n 2048  -o 0 ./teamscatter 256000  -c 4 #-nocheck
 
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 8 #-nocheck
+ibrun -n 2048  -o 0 ./teamscatter 256000  -c 8 #-nocheck
 
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 16 #-nocheck
+ibrun -n 2048  -o 0 ./teamscatter 256000  -c 16 #-nocheck
 
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 32 #-nocheck
-
-ibrun -n 4096  -o 0 ./teamscatter 256000  -c 64 #-nocheck
+ibrun -n 2048  -o 0 ./teamscatter 256000  -c 32 #-nocheck
